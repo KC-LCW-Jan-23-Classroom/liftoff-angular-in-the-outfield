f345350c03c1bd5d484b998700c02082a4d31d4e
Elizabeth Karr <e.karr93@icloud.com>
2023-10-09 08:57:34 -0500

    generate components, model, and service

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieItemComponent } from './movie-list/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
