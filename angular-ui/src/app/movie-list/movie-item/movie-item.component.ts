b36922979fc16bee6d1d0ee00978b8b371534992
Elizabeth Karr <e.karr93@icloud.com>
2023-10-09 20:41:18 -0500

    mock up movie-item

--- angular-ui/src/app/movie-list/movie-item/movie-item.component.ts
+++ angular-ui/src/app/movie-list/movie-item/movie-item.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
-  styleUrls: ['./movie-item.component.css']
+  styleUrls: ['./movie-item.component.css'],
 })
 export class MovieItemComponent {
-
+  movieItem: Movie = new Movie(
+    'Peter Pan',
+    ['Adventure', 'Family', 'Fantasy', 'Romance'],
+    'PG',
+    2003,
+    113,
+    '/assets/images/posters/peter-pan.png',
+    'In stifling Edwardian London, Wendy Darling mesmerizes her brothers every night with bedtime tales of swordplay, swashbuckling, and the fearsome Captain Hook. But the children become the heroes of an even greater story, when Peter Pan flies into their nursery one night and leads them over moonlit rooftops through a galaxy of stars and to the lush jungles of Neverland. Wendy and her brothers join Peter and the Lost Boys in an exhilarating life--free of grown-up rules--while also facing the inevitable showdown with Hook and his bloodthirsty pirates.',
+    'P.J. Hogan',
+    ['Jeremy Sumpter', 'Jason Isaacs', 'Olivia Williams'],
+    ['Prime']
+  );
 }
--- angular-ui/src/app/movie-list/movie.model.ts
+++ angular-ui/src/app/movie-list/movie.model.ts
@@ -1,2 +1,37 @@
 export class Movie {
+  public title: string;
+  public genres: string[];
+  public rating: string;
+  public releaseYear: number;
+  public runtimeMinutes: number;
+  public poster: string;
+  public plot: string;
+  public director: string;
+  public stars: string[];
+  public streamingSources: string[];
+
+  constructor(
+    title: string,
+    genres: string[],
+    rating: string,
+    releaseYear: number,
+    runtimeMinutes: number,
+    poster: string,
+    plot: string,
+    director: string,
+    stars: string[],
+    streamingSources: string[]
+  ) {
+    this.title = title;
+    this.genres = genres;
+    this.rating = rating;
+    this.rating = rating;
+    this.releaseYear = releaseYear;
+    this.runtimeMinutes = runtimeMinutes;
+    this.poster = poster;
+    this.plot = plot;
+    this.streamingSources = streamingSources;
+    this.director = director;
+    this.stars = stars;
+  }
 }
