718c34bc0cd1cb2cde8e07a458c529912df62f41
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2023-10-10 21:50:38 -0500

    created navbar

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component'; // Import the NavbarComponent here
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent, // Declare NavbarComponent in the declarations array
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [BrowserModule, AppRoutingModule, NavbarComponent],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
