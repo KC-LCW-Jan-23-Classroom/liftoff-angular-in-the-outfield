82e2f35ae81d63453d855c28546aafb40993307a
Aaron Alexander <104107484+HiddenBrilliance@users.noreply.github.com>
2023-10-11 18:49:19 -0500

    made navbar

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -14,19 +14,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { NavbarComponent } from './navbar/navbar.component';
-
-// @NgModule({
-//   declarations: [AppComponent],
-//   imports: [BrowserModule, AppRoutingModule, NavbarComponent],
-//   providers: [],
-//   bootstrap: [AppComponent],
-// })
-// export class AppModule {}
+export class AppModule {};
