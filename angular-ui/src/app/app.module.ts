0cb078ad44e02d2f56580c6152175efa0cc855b8
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-10-11 21:46:25 -0500

    component

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieItemComponent } from './movie-list/movie-item/movie-item.component';
+import { BrowseOptionComponent } from './browse-option/browse-option.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MovieItemComponent } from './movie-list/movie-item/movie-item.component
     AppComponent,
     SidebarComponent
     MovieListComponent,
-    MovieItemComponent
+    MovieItemComponent,
+    BrowseOptionComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-ui/src/app/browse-option/browse-option.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-option',
+  templateUrl: './browse-option.component.html',
+  styleUrls: ['./browse-option.component.css']
+})
+export class BrowseOptionComponent {
+
+}
