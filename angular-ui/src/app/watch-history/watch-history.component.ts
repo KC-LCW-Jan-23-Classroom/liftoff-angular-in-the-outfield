316c31f8073281f256ad32422a1dce36d3443866
ErinKace <erinkacerovskis@gmail.com>
2023-10-14 15:33:09 -0500

    I tried to get started on the logic of getting data but need to know how it fits into the app better first

--- angular-ui/src/app/watch-history/watch-history.component.ts
+++ angular-ui/src/app/watch-history/watch-history.component.ts
@@ -9,4 +9,9 @@ import { Movie } from '../movie-list/movie.model';
 export class WatchHistoryComponent {
   watchedList: Movie[] = [];
 
+  // call backend to recieve a list of movie Ids that the user has added to their watch list
+
+  // call API with the list of movie Ids to 
+  //
+
 }
