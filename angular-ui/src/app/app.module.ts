d3a6ef35c7e234216b35355bb886043844268173
ErinKace <erinkacerovskis@gmail.com>
2023-10-15 13:38:53 -0500

    added file

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -27,7 +27,7 @@ import { DatePipe } from '@angular/common';
     MovieItemComponent,
     BrowseOptionComponent,
 
-    TimerComponent
+    TimerComponent,
 
     WatchHistoryComponent,
     FooterComponent,
