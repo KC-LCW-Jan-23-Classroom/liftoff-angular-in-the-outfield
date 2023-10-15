351949ea2d11f26bccfc62d4747dd23cc439e73a
ErinKace <erinkacerovskis@gmail.com>
2023-10-15 11:43:55 -0500

    working on typescript settimer function

--- angular-ui/src/app/sidebar/timer/timer.component.ts
+++ angular-ui/src/app/sidebar/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from '../../movie-list/movie.model';
 
 @Component({
   selector: 'app-timer',
@@ -9,8 +10,22 @@ export class TimerComponent {
 
   timesUp: boolean = false;
 
+  randomMovie: Movie = new Movie(
+    1,
+    'Peter Pan',
+    ['Adventure', 'Family', 'Fantasy', 'Romance'],
+    'PG',
+    2003,
+    113,
+    '/assets/images/posters/peter-pan.png',
+    'In stifling Edwardian London, Wendy Darling mesmerizes her brothers every night with bedtime tales of swordplay, swashbuckling, and the fearsome Captain Hook. But the children become the heroes of an even greater story, when Peter Pan flies into their nursery one night and leads them over moonlit rooftops through a galaxy of stars and to the lush jungles of Neverland. Wendy and her brothers join Peter and the Lost Boys in an exhilarating life--free of grown-up rules--while also facing the inevitable showdown with Hook and his bloodthirsty pirates.',
+    'P.J. Hogan',
+    ['Jeremy Sumpter', 'Jason Isaacs', 'Olivia Williams'],
+    ['Prime']
+  );
+
   startTimer() {
-    
+    setTimeout(()=> {this.timesUp=true;}, 1000);
   }
 
 }
