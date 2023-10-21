2917582339f105dac84a561c6fd72166494e0702
Elizabeth Karr <e.karr93@icloud.com>
2023-10-21 18:02:35 -0500

    refactor functions so they call api once api key is returned from backend

--- angular-ui/src/app/shared/movies.service.ts
+++ angular-ui/src/app/shared/movies.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map, forkJoin } from 'rxjs';
+import { Observable, map, forkJoin, switchMap, zip } from 'rxjs';
 import { Movie } from './movie.model';
+import { ApikeyService } from './apikey.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,74 +10,76 @@ import { Movie } from './movie.model';
 export class MoviesService {
   private apiUrl: string = 'https://api.themoviedb.org/3/';
   private apiKey!: string;
-  private options = { params: new HttpParams().set('api_key', this.apiKey) };
 
-  constructor(private http: HttpClient) {
-    this.http
-      .get<{ apiKey: string }>('http://localhost:8080/api/get-api-key')
-      .subscribe((data) => {
-        this.apiKey = data.apiKey;
-      });
+  constructor(private http: HttpClient, private apiKeyService: ApikeyService) {
+    this.apiKeyService
+      .getApiKey()
+      .subscribe((apiKey) => (this.apiKey = apiKey));
   }
 
   fetchTrendingMoviesIds(): Observable<number[]> {
-    // Make the API request to get trending movies with a limit of 20
-    const url = `${this.apiUrl}/trending/movie/day?language=en-US&page=1&total_results=20`;
-    return this.http.get<any>(url, this.options).pipe(
-      // Map the response to extract movie IDs
-      map((response) =>
-        response.results.slice(0, 20).map((movie: any) => movie.id)
-      )
+    return this.apiKeyService.getApiKey().pipe(
+      switchMap((apiKey) => {
+        const url = `${this.apiUrl}/trending/movie/day?language=en-US&page=1&total_results=20&api_key=${apiKey}`;
+        return this.http.get<any>(url).pipe(
+          map((response) =>
+            response.results.slice(0, 20).map((movie: any) => movie.id)
+          )
+        );
+      })
     );
   }
 
   fetchMovieListDetails(movieIds: number[]): Observable<Movie[]> {
-    const movieObservables = movieIds.map((movieId) => {
-      const movieUrl = `${this.apiUrl}/movie/${movieId}?append_to_response=watch%2Fproviders&language=en-US&api_key=${this.apiKey}`;
-      const directorAndCast$ = this.getDirectorAndCast(movieId);
+    return this.apiKeyService.getApiKey().pipe(
+      switchMap((apiKey) => {
+        const movieObservables = movieIds.map((movieId) => {
+          const movieUrl = `${this.apiUrl}/movie/${movieId}?append_to_response=watch%2Fproviders&language=en-US&api_key=${apiKey}`;
+          const directorAndCast$ = this.getDirectorAndCast(movieId);
 
-      return forkJoin([this.http.get<any>(movieUrl), directorAndCast$]).pipe(
-        map((responses: any[]) => {
-          const movieResponse = responses[0];
-          const directorAndCast = responses[1];
+          return zip(
+            this.http.get<any>(movieUrl),
+            directorAndCast$,
+            (movieResponse, directorAndCast) => {
+              let streamingSources: string[] | undefined;
 
-          let streamingSources: string[] | undefined;
+              if (
+                movieResponse['watch/providers'] &&
+                movieResponse['watch/providers'].results &&
+                movieResponse['watch/providers'].results.US
+              ) {
+                const flatrate =
+                  movieResponse['watch/providers'].results.US.flatrate;
+                if (flatrate) {
+                  streamingSources = flatrate.map((source: any) =>
+                    this.getLogoPath(source.logo_path)
+                  );
+                }
+              }
 
-          if (
-            movieResponse['watch/providers'] &&
-            movieResponse['watch/providers'].results &&
-            movieResponse['watch/providers'].results.US
-          ) {
-            const flatrate =
-              movieResponse['watch/providers'].results.US.flatrate;
-            if (flatrate) {
-              streamingSources = flatrate.map((source: any) =>
-                this.getLogoPath(source.logo_path)
+              const posterPath = this.getImagePath(movieResponse.poster_path);
+
+              return new Movie(
+                movieResponse.id,
+                movieResponse.title,
+                movieResponse.genres.map((genre: any) => genre.name),
+                new Date(movieResponse.release_date).getFullYear(),
+                movieResponse.runtime,
+                posterPath,
+                movieResponse.overview,
+                directorAndCast.director,
+                directorAndCast.cast,
+                streamingSources && streamingSources.length > 0
+                  ? streamingSources
+                  : undefined
               );
             }
-          }
-
-          const posterPath = this.getImagePath(movieResponse.poster_path);
-
-          return new Movie(
-            movieResponse.id,
-            movieResponse.title,
-            movieResponse.genres.map((genre: any) => genre.name),
-            new Date(movieResponse.release_date).getFullYear(),
-            movieResponse.runtime,
-            posterPath,
-            movieResponse.overview,
-            directorAndCast.director,
-            directorAndCast.cast,
-            streamingSources && streamingSources.length > 0
-              ? streamingSources
-              : undefined
           );
-        })
-      );
-    });
+        });
 
-    return forkJoin(movieObservables);
+        return forkJoin(movieObservables);
+      })
+    );
   }
 
   getImagePath(posterPath: string | null): string | null {
@@ -96,19 +99,25 @@ export class MoviesService {
   getDirectorAndCast(
     movieId: number
   ): Observable<{ director: string; cast: string[] }> {
-    const creditsUrl = `${this.apiUrl}/movie/${movieId}/credits`;
+    return this.apiKeyService.getApiKey().pipe(
+      switchMap((apiKey) => {
+        const creditsUrl = `${this.apiUrl}/movie/${movieId}/credits?api_key=${apiKey}`;
 
-    return this.http.get<any>(creditsUrl, this.options).pipe(
-      map((response) => {
-        const director = response.crew.find(
-          (member: any) => member.job === 'Director'
+        return this.http.get<any>(creditsUrl).pipe(
+          map((response) => {
+            const director = response.crew.find(
+              (member: any) => member.job === 'Director'
+            );
+            const cast = response.cast
+              .slice(0, 5)
+              .map((actor: any) => actor.name);
+
+            return {
+              director: director ? director.name : 'N/A',
+              cast,
+            };
+          })
         );
-        const cast = response.cast.slice(0, 5).map((actor: any) => actor.name);
-
-        return {
-          director: director ? director.name : 'N/A',
-          cast,
-        };
       })
     );
   }
