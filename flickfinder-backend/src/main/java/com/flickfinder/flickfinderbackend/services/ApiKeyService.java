75defbb3d7b3509aad125b3128563216461bbd99
Elizabeth Karr <e.karr93@icloud.com>
2023-10-21 16:27:22 -0500

    add api key to application.properties and create service to retrieve key

---
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/services/ApiKeyService.java
@@ -0,0 +1,15 @@
+package com.flickfinder.flickfinderbackend.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiKeyService {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+}
