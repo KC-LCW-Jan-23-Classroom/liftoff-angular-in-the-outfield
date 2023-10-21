9dc3615a1514cad8a093b4e65662f4d8cad73d95
Elizabeth Karr <e.karr93@icloud.com>
2023-10-21 18:02:09 -0500

    refactor to read json object returned from springboot

--- angular-ui/src/app/shared/apikey.service.ts
+++ angular-ui/src/app/shared/apikey.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApikeyService {
+  private apiKey$: Observable<string>;
+  private url: string = 'http://localhost:8080/api/get-api-key';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiKey$ = this.http.get<{apiKey: string}>(this.url).pipe(
+      map((response) => response.apiKey)
+    );
+  }
+
+  getApiKey(): Observable<string> {
+    return this.apiKey$;
+  }
 }
