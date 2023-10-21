3afb8ca4837d8dfec76a5a099542cd5015801a29
ErinKace <erinkacerovskis@gmail.com>
2023-10-21 17:34:47 -0500

    began working on users.service

---
+++ angular-ui/src/app/users.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private url = 'LocalHost:8080'
+  // private currentUser : User;
+
+  constructor(private http: HttpClient) { }
+}
