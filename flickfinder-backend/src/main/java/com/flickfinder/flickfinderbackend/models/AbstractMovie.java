fd2e33bd58699a2a3b9e24937bd9b7c07905582d
ErinKace <erinkacerovskis@gmail.com>
2023-10-20 21:41:49 -0500

    added watchedmovie class

--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/models/AbstractMovie.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/models/AbstractMovie.java
@@ -1,6 +1,14 @@
 package com.flickfinder.flickfinderbackend.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@MappedSuperclass
 public abstract class AbstractMovie {
+    @Id
+    @GeneratedValue
     private int movieId;
 
     public AbstractMovie() {
@@ -14,7 +22,15 @@ public abstract class AbstractMovie {
         return movieId;
     }
 
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractMovie that)) return false;
+        return movieId == that.movieId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieId);
     }
 }
---
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/models/WatchedMovie.java
@@ -0,0 +1,11 @@
+package com.flickfinder.flickfinderbackend.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class WatchedMovie extends AbstractMovie {
+
+    public WatchedMovie() {
+        super();
+    }
+}
