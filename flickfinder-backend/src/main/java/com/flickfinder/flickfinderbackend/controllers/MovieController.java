4aa13a7b8cda72f2551e83b606702a454f6cc929
ErinKace <erinkacerovskis@gmail.com>
2023-10-22 12:10:00 -0500

    working on movie controller

--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/MovieController.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/MovieController.java
@@ -1,8 +1,26 @@
 package com.flickfinder.flickfinderbackend.controllers;
 
+import com.flickfinder.flickfinderbackend.models.WatchedMovie;
+import com.flickfinder.flickfinderbackend.models.data.WatchedMovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
+
+    @Autowired
+    WatchedMovieRepository watchHistoryRepository;
+
+    //TODO post watch list based on user logged in
+    public List<WatchedMovie> displayWatchList() {
+
+        return (List<WatchedMovie>) watchHistoryRepository.findAll();
+    }
+
+    //TODO get new movie to add to watch list
+
+
 
 }
