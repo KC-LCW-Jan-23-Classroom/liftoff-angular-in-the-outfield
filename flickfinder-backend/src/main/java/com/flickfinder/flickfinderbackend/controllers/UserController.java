d88da630ec9d9cb3aed8dd525c5fc9d65f357767
Harmony Love <harmonylove427@gmail.com>
2023-10-22 11:59:32 -0500

    save changes to UserController and UserRepository, Database and UserRepo seem to be functional

--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/UserController.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/UserController.java
@@ -4,7 +4,6 @@ import com.flickfinder.flickfinderbackend.models.data.UserRepository;
 import com.flickfinder.flickfinderbackend.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/models/data/UserRepository.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/models/data/UserRepository.java
@@ -1,7 +1,6 @@
 package com.flickfinder.flickfinderbackend.models.data;
 
 import com.flickfinder.flickfinderbackend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
