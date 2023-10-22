8785b2cb79168a4bc838e3e89f6858c9f43881f0
ErinKace <erinkacerovskis@gmail.com>
2023-10-22 12:50:19 -0500

    still trying to figure out rest controllers

--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/MovieController.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/MovieController.java
@@ -3,20 +3,24 @@ package com.flickfinder.flickfinderbackend.controllers;
 import com.flickfinder.flickfinderbackend.models.WatchedMovie;
 import com.flickfinder.flickfinderbackend.models.data.WatchedMovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("movies")
 public class MovieController {
 
     @Autowired
     WatchedMovieRepository watchHistoryRepository;
 
     //TODO post watch list based on user logged in
-    public List<WatchedMovie> displayWatchList() {
 
-        return (List<WatchedMovie>) watchHistoryRepository.findAll();
+    @PostMapping("{userId}/watch-history")
+    @ResponseBody
+    public ResponseEntity<List<WatchedMovie>> displayWatchList(@PathVariable int userId) {
+
+
     }
 
     //TODO get new movie to add to watch list
