fa9dbee8b1d043e19a75e354bc19555461dcc34c
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-10-22 12:48:24 -0500

    update controller

--- flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controller/MovieController.java
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controller/MovieController.java
@@ -1,2 +1,21 @@
-package com.flickfinder.flickfinderbackend.controller;public class MovieController {
+package com.flickfinder.flickfinderbackend.controller;
+public class MovieController {
+}
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+    @GetMapping("/movies")
+    public List<Movie> getMovies() {
+        // Return a list of available movies
+    }
+
+    // @PostMapping("/rate")
+    // public void rateMovie(@RequestBody Rating rating) {
+    //     // Store user movie ratings
+    // }
+
+    @PostMapping("/recommendations")
+    public List<Movie> getRecommendations(@RequestBody Movie movie) {
+        // Implement recommendation algorithm and return recommended movies
+    }
 }
