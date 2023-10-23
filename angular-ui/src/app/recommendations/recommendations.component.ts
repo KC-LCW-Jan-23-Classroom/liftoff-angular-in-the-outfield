4ad9137a03a42fc9e76b1a5d681479a809d617e1
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-10-23 13:12:52 -0500

    update

--- angular-ui/src/app/recommendations/recommendations.component.ts
+++ angular-ui/src/app/recommendations/recommendations.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+export interface Movie {
+  id: number;
+  title: string;
+  director: string;
+  releaseYear: number;
+  genre: string;
+}
 
 @Component({
-  selector: 'app-recommendations',
+  selector: 'app-movie-list',
   templateUrl: './recommendations.component.html',
   styleUrls: ['./recommendations.component.css']
 })
-export class RecommendationsComponent {
+export class RecommendationsComponent implements OnInit {
+  selectedMovie: Movie | undefined;
+  movies: Movie[] = [];
+  recommendedMovies: Movie[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Fetch the list of movies from your API.
+    this.http.get<Movie[]>('/api/movies').subscribe((data: Movie[]) => {
+      this.movies = data;
+    });
+  }
+
+  getRecommendations() {
+    if (this.selectedMovie) {
+     // Send a request to get movie recommendations based on the selected movie.
+      this.http.post<Movie[]>('/api/recommendations', { movie: this.selectedMovie }).subscribe((data: Movie[]) => {
+        this.recommendedMovies = data;
+      });
+    }
+  }
 }
