592be6439bc4344007e006f5a5c345460d2d4dfd
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-11-04 18:16:56 -0500

    updates

--- angular-ui/src/app/sidebar/recommendations/recommendations.component.ts
+++ angular-ui/src/app/sidebar/recommendations/recommendations.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '../../shared/movie.model';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { ApikeyService } from '../../shared/apikey.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-recommendations',
@@ -10,54 +10,57 @@ import { ApikeyService } from '../../shared/apikey.service';
   styleUrls: ['./recommendations.component.css'],
   animations: [
     trigger('spin', [
-      state('void', style({ transform: 'rotate(0)' })),
-      transition('* => *', [
-        animate('1s', keyframes([
-          style({ transform: 'rotate(0)', offset: 0 }),
-          style({ transform: 'rotate(180deg)', offset: 0.5 }),
-          style({ transform: 'rotate(360deg)', offset: 1 })
-        ]))
-      ])
-    ])
-  ]
+      state('start', style({ transform: 'rotate(0)' })),
+      state('end', style({ transform: 'rotate(360deg)' })),
+      transition('start <=> end', animate('1s ease-out')),
+    ]),
+  ],
 })
+
 export class RecommendationsComponent implements OnInit {
   apiUrl: string = 'https://api.themoviedb.org/3/movie/${this.selectedMovie.id}/similar?api_key=${this.apiKeyService.getApiKey()}&language=en-US&page=1&include_adult=false`;';
+  spinAnimationState: string = 'start'; // Initial animation state  is 'start'
 
-  constructor(private http: HttpClient, private apiKeyService: ApikeyService) {}
 
-  selectedMovie: Movie | undefined;
-  movies: Movie[] = [];
-  recommendedMovies: Movie[] = [];
+    constructor(private http: HttpClient, private apiKeyService: ApikeyService) {}
 
-  ngOnInit() {
-    this.movies = [
-      {
-        id: 1,
-        title: 'Movie 1',
-        director: 'Director 1',
-        releaseYear: 2020,
-        runtimeMinutes: 120,
-        poster: 'https://image.tmdb.org/t/p/w500/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg',
-        plot: 'Plot 1',
-        streamingSources: ['Netflix', 'Hulu'],
-        cast: ['Actor 1', 'Actor 2'],
-        genres: ['Thriller'], // Define genres as an array even if it contains a single genre
-      }
+    selectedMovie: Movie | undefined;
+    movies: Movie[] = [];
+    recommendedMovies: Movie[] = [];
 
-    ];
+    ngOnInit() {
+      this.movies = [
+        {
+          id: 1,
+          title: 'Movie 1',
+          director: 'Director 1',
+          releaseYear: 2020,
+          runtimeMinutes: 120,
+          poster: 'https://image.tmdb.org/t/p/w500/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg',
+          plot: 'Plot 1',
+          streamingSources: ['Netflix', 'Hulu'],
+          cast: ['Actor 1', 'Actor 2'],
+          genres: ['Thriller'], // Define genres as an array even if it contains a single genre
+        },
 //logic for get recommendations here
-    this.selectedMovie = this.movies[0];
-    this.getRecommendations();
-  }
-
-  getRecommendations() {
-    if (this.selectedMovie?.id) {
-      this.apiUrl = `https://api.themoviedb.org/3/movie/${this.selectedMovie.id}/similar?api_key=${this.apiKeyService.getApiKey()}&language=en-US&page=1&include_adult=false`;
-
-      this.http.get<Movie[]>(this.apiUrl).subscribe((data: Movie[]) => {
-        this.recommendedMovies = data;
-      });
+      ];
     }
+
+getRecommendations() {
+  if (this.selectedMovie?.id) {
+    this.apiUrl = `https://api.themoviedb.org/3/movie/${this.selectedMovie.id}/similar?api_key=${this.apiKeyService.getApiKey()}&language=en-US&page=1&include_adult=false`;
+
+    this.http.get<any>(this.apiUrl).subscribe((data: any) => {
+      this.recommendedMovies = data.results; // Update recommendedMovies with API response
+    }, error => {
+      console.error('Error fetching recommendations:', error);
+    });
   }
 }
+
+startSpinAnimation() {
+  this.spinAnimationState = this.spinAnimationState === 'start' ? 'end' : 'start';
+}
+}
+
+
