2369ffcf7be8f9623067d7fecb4a747afb76224d
ErinKace <erinkacerovskis@gmail.com>
2023-11-08 14:50:03 -0600

    fixed problems with my watch history and my list components

--- angular-ui/src/app/my-list/my-list.component.ts
+++ angular-ui/src/app/my-list/my-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Movie } from '../shared/movie.model';
+import { UsersService } from '../shared/users.service';
+import { MoviesService } from '../shared/movies.service';
 
 @Component({
   selector: 'app-my-list',
   templateUrl: './my-list.component.html',
   styleUrls: ['./my-list.component.css']
 })
-export class MyListComponent {
+export class MyListComponent implements OnInit {
   myList: Movie[] = [];
+
+  constructor(private usersService: UsersService, private moviesService : MoviesService) {
+  }
+  
+  ngOnInit(): void {
+    
+  }
+  
 
 }
