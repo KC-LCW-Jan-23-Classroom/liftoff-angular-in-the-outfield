fb0be309fa23da1e742b5a7e40f311382ef0bd5a
Harmony Love <harmonylove427@gmail.com>
2023-11-08 20:03:12 -0600

    register page funtionalgit add .

--- angular-ui/src/app/user-register/user-register.component.ts
+++ angular-ui/src/app/user-register/user-register.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service';
 import { User } from '../user/user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+
+
 
 @Component({
   selector: 'app-user-register',
@@ -9,23 +15,29 @@ import { User } from '../user/user';
 })
 export class UserRegisterComponent implements OnInit {
   isLoggedIn = false;
-  user!: User;
+  user: User = new User(0, '', '', '', '');
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){
+    
+  }
 
   ngOnInit(): void {
     this.authService.popcornLogin();
     };
+
     onSubmit(): void {
-      // this.authService.registerUser(this.user).subscribe(
-      //   response => {
-      //     console.log('Registration successful');
-      //   },
-      //   error => {
-      //     console.error('Registration failed', error);
-      //   }
-      //);
-      console.log("Click me")
-      //function to direct register to submit or console log temporarily
+      this.authService.registerUser(this.user).subscribe({
+        next: (response: any) => {
+          console.log('Registration successful');
+          if (response.id) {
+            this.router.navigate(['/login']);
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('Registration failed', error);
+        },
+      });
+      
+      
     }
 }
--- angular-ui/src/app/user/user.ts
+++ angular-ui/src/app/user/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    id: number;
+    id?: number;
     name: string;
     email: string;
     password: string;
