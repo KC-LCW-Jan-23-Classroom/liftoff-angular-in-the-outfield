44b7e4548b3b4c83cf6782a3347df03ef3634bb4
ErinKace <erinkacerovskis@gmail.com>
2023-11-18 16:38:07 -0600

    no closer than when I started

--- angular-ui/src/app/shared/users.service.ts
+++ angular-ui/src/app/shared/users.service.ts
@@ -3,8 +3,10 @@ import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SavedMovie } from './saved-movie.model';
 import { Movie } from './movie.model';
-import { User } from './user.model';
+import { User } from '../user/user';
 import { MoviesService } from './movies.service';
+import { UserLoginComponent } from '../user-login/user-login.component';
+import { AuthService } from '../user/auth.service';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -20,17 +22,18 @@ export class UsersService implements OnInit {
 
   private backendUrl = 'http://localhost:8080/'
   private currentUserId = 1;
+  private loggedIn : boolean = false;
   private watchHistory : Movie[] = [];
   private myList : Movie[] = [];
 
 
   //TODO in login method, set currentUserId
 
-  constructor(private http: HttpClient, moviesService: MoviesService) { }
+  constructor(private http: HttpClient, moviesService: MoviesService, private authService: AuthService) { }
 
 
   ngOnInit(): void {
-    this.fetchWatchHistory();
+
   }
 
   fetchWatchHistory(): Observable<number[]> {
