87b82709620800441c0e63a98b3e17202bf5d883
Harmony Love <harmonylove427@gmail.com>
2023-11-26 14:54:40 -0600

    commit changes for user session and token keys for session, not fully functional for merge, but its somewhat working for login

--- angular-ui/src/app/user/auth.service.ts
+++ angular-ui/src/app/user/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import { User } from './user';
 import { NavbarComponent } from '../navbar/navbar.component';
 
@@ -13,6 +13,7 @@ export class AuthService {
   private authStatusSubject = new Subject<boolean>();
   private registerUrl = 'http://localhost:8080/auth/register';
   private authUrl = 'http://localhost:8080/auth/login';
+  private tokenKey = 'authToken'; // Added tokenKey
 
 
   constructor(private http: HttpClient) { 
@@ -53,24 +54,44 @@ getVarResponse(){
     this.varResponse = setWith;
   }
 
+ // New method to check if the user is authenticated
+ isAuthenticated(): boolean {
+  const token = localStorage.getItem(this.tokenKey);
+  return !!token;
+}
 
+// Logout method
+logout(): void {
+  localStorage.removeItem(this.tokenKey);
+  // Clear other user-related data if needed
+}
   
   
 
     checkLoggedIn(): Observable<boolean> {
-      if (this.varResponse) {
-        // If the user is logged in, return an observable for login status.
-        return this.http.get<boolean>(this.authUrl);
+      const token = localStorage.getItem(this.tokenKey);
+      if (token) {
+        // If the token exists, the user is considered logged in
+        return of(true);
       } else {
-        // If the user is not logged in, return an observable for registration status.
-        return this.http.get<boolean>(this.registerUrl);
+        // If the token doesn't exist, the user is considered not logged in
+        return of(false);
       }
+      // if (this.varResponse) {
+      //   // If the user is logged in, return an observable for login status.
+      //   return this.http.get<boolean>(this.authUrl);
+      // } else {
+      //   // If the user is not logged in, return an observable for registration status.
+      //   return this.http.get<boolean>(this.registerUrl);
+      // }
     }
 
   public popcornLogin(): void {
     this.loginUser(this.user).subscribe({
       next:(response:boolean) => {
        this.varResponse = response;
+       this.setAuthStatus(response); // Set the authentication status
+
        console.log("LoginCheck here")
       },
       error: (error: any) => {
