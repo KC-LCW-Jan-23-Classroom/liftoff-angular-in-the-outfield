6eb7843399d13cd3289b502b8ae8fd3c820e128a
ErinKace <erinkacerovskis@gmail.com>
2024-01-08 20:12:43 -0600

    added button components

---
+++ angular-ui/src/app/shared/saved-button/saved-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-button',
+  templateUrl: './saved-button.component.html',
+  styleUrls: ['./saved-button.component.css']
+})
+export class SavedButtonComponent {
+
+}
---
+++ angular-ui/src/app/shared/watched-button/watched-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watched-button',
+  templateUrl: './watched-button.component.html',
+  styleUrls: ['./watched-button.component.css']
+})
+export class WatchedButtonComponent {
+
+}
